c637492fc30333515c8d272b1353023ca980186f
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2017-09-17 18:48:13 -0700

    different spacing for different elements and clean up the code a bit

--- apps/login_app/static/login_app/javascript/index.js
+++ apps/login_app/static/login_app/javascript/index.js
@@ -20,7 +20,46 @@ $(function(){
     fadeElem.each(function(){
       var $this = $(this),
         elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/200);
+    });
+  });
+});
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#mobilebandcampplayer");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/150);
+    });
+  });
+});
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#bandcampplayer");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+    });
+  });
+});
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#formcontainer");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
     });
   });
 });
